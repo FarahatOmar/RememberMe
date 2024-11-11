66148932177627ac4a532ae48ea146d09ee4278c
omarProgrammer21 <o214729@gmail.com>
2024-11-11 22:10:23 +0200

    server.js Set a Timeout

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,9 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI, {
+  serverSelectionTimeoutMS: 5000,
+});
 
 // Routes
 const namesRoute = require("./routes/names");
